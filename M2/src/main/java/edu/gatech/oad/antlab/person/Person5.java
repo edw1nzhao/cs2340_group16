6da28bcd3db3f227000bb264dbd02e93099eaa6c
Edwin Zhao <edwin.zhao@outlook.com>
2017-01-21 14:19:20 -0500

    Completed P5

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,19 +31,30 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+		String temp = "";
+
+		int length = input.length();    //4
+
+		for (int i = 0; i < length; i++) {
+		    if ((i + 2) >= (length)) {
+		        temp += input.charAt(i - length + 2);
+		    } else {
+		        temp += input.charAt(i + 2);
+		    }
+		}
+
+		return temp;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
-
 }
