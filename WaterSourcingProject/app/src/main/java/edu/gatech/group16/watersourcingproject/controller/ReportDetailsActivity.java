31dfeb1f9afbc1780f5e163ce29c86117ca20cf1
tomonarifeehan <tfeehan3@gatech.edu>
2017-02-24 12:28:22 -0500

    Added ReportDetailsActivity

---
+++ WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/ReportDetailsActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.group16.watersourcingproject.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.gatech.group16.watersourcingproject.R;
+public class ReportDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_details);
+    }
+}
