7c9d11926324c745fd49a72a7a7dcdea40a6d852
tomonarifeehan <tfeehan3@gatech.edu>
2017-02-28 13:12:32 -0500

    Added methods and completed compileReport() in NewWaterSourceReport

--- WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/NewWaterSourceReport.java
+++ WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/NewWaterSourceReport.java
@@ -2,6 +2,7 @@ package edu.gatech.group16.watersourcingproject.controller;
 
 import android.content.Intent;
 import android.icu.text.DateFormat;
+import android.location.Location;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import android.os.Bundle;
 import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.ArrayAdapter;
-
+import android.widget.Spinner;
 
 import java.util.Date;
 import java.util.List;
@@ -17,6 +18,8 @@ import java.util.List;
 import edu.gatech.group16.watersourcingproject.R;
 import edu.gatech.group16.watersourcingproject.controller.HomeActivity;
 import edu.gatech.group16.watersourcingproject.model.Enums.AccountType;
+import edu.gatech.group16.watersourcingproject.model.Enums.WaterCondition;
+import edu.gatech.group16.watersourcingproject.model.Enums.WaterType;
 import edu.gatech.group16.watersourcingproject.model.User;
 import edu.gatech.group16.watersourcingproject.model.WaterSourceReport;
 
@@ -25,6 +28,7 @@ public class NewWaterSourceReport extends AppCompatActivity implements OnClickLi
     private User user;
     private String currentDateTimeString;
     private List<WaterSourceReport> wsReports;
+    private Spinner waterType, waterCondition;
 
     /**
      * OnCreate method required to load activity and loads everything that
@@ -49,6 +53,9 @@ public class NewWaterSourceReport extends AppCompatActivity implements OnClickLi
 
         findViewById(R.id.edit_button_cancel).setOnClickListener(this);
         findViewById(R.id.edit_button_save).setOnClickListener(this);
+
+        waterType = (Spinner) findViewById(R.id.spinner_watertype);
+        waterCondition = (Spinner) findViewById(R.id.spinner_watercondition);
     }
 
     /**
@@ -85,9 +92,25 @@ public class NewWaterSourceReport extends AppCompatActivity implements OnClickLi
      */
     @RequiresApi(api = Build.VERSION_CODES.N)
     public WaterSourceReport compileReport() {
-        currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-
-        WaterSourceReport wsReport = new WaterSourceReport();
+        int reportNumber = getReportNumber();
+        Date currentDate = new Date();
+        Location location = getUserLocation();
+        WaterType type = (WaterType) waterType.getSelectedItem();
+        WaterCondition condition = (WaterCondition) waterCondition.getSelectedItem();
+        String submittedBy = user.getName();
+        WaterSourceReport wsReport = new WaterSourceReport(reportNumber, currentDate, location, type, condition, submittedBy);
         return wsReport;
+    }
+    public static int getReportNumber() {
+        //TO DO: Retrieve number of reports from Firebase variable.
+        return 0;
+    }
+
+    public static Location getUserLocation() {
+        //TO DO: Retrieve actual location from phone.
+        Location location = new Location("Temp Location");
+        location.setLatitude(1.2345d);
+        location.setLongitude(1.2345d);
+        return location;
     }
 }
