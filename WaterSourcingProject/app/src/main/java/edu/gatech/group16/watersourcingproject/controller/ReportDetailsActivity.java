c656540b29c86357530ecee575ec17701b9d327b
tomonarifeehan <tfeehan3@gatech.edu>
2017-02-28 20:55:38 -0500

    Wrote functionality for displaying individual report details.

--- WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/ReportDetailsActivity.java
+++ WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/ReportDetailsActivity.java
@@ -1,11 +1,22 @@
 package edu.gatech.group16.watersourcingproject.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 import edu.gatech.group16.watersourcingproject.R;
 import edu.gatech.group16.watersourcingproject.model.User;
+import edu.gatech.group16.watersourcingproject.model.WaterSourceReport;
 
 public class ReportDetailsActivity extends AppCompatActivity {
     private User user;
+    private TextView reportNumber;
+    private TextView reportDate;
+    private TextView reportLocation;
+    private TextView reportWaterType;
+    private TextView reportAuthor;
+    private int position;
     /**
      * OnCreate method required to load activity and loads everything that
      * is needed for the page while setting the view.
@@ -20,6 +31,20 @@ public class ReportDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_report_details);
 
         user = (User) getIntent().getSerializableExtra("USER");
+        position =  (int) getIntent().getSerializableExtra("POSITION");
 
+        reportNumber = (TextView) findViewById(R.id.label_reportNum);
+        reportDate = (TextView) findViewById(R.id.label_reportDate);
+        reportLocation = (TextView) findViewById(R.id.label_reportLoc);
+        reportWaterType = (TextView) findViewById(R.id.label_reportWT);
+        reportAuthor = (TextView) findViewById(R.id.label_reportAuth);
+
+        for (WaterSourceReport item: user.getWaterSourceReport()) {
+            reportDate.setText(item.getDate().toString());
+            reportLocation.setText(item.getLocation().toString());
+            reportWaterType.setText(item.getWaterType().toString());
+        }
+        reportNumber.setText(position);
+        reportAuthor.setText(user.getName());
     }
 }
