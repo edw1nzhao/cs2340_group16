65fddf560f00b8c53fc30f51c5771fab79c2a17a
tomonarifeehan <tfeehan3@gatech.edu>
2017-03-23 00:35:15 -0400

    Added functioning demo of graph plot in HistoricalReportActivity.java

--- WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/HistoricalReportActivity.java
+++ WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/HistoricalReportActivity.java
@@ -1,20 +1,74 @@
 package edu.gatech.group16.watersourcingproject.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.text.Text;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import edu.gatech.group16.watersourcingproject.R;
+import edu.gatech.group16.watersourcingproject.model.User;
 
 public class HistoricalReportActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TextView graphInfo;
+    private User user;
+    private String selectedPPM, latitude, longitude;
+    private int selectedYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historical_report);
+        user = (User) getIntent().getSerializableExtra("USER");
+        latitude = getIntent().getExtras().getString("LATITUDE");
+        longitude = getIntent().getExtras().getString("LONGITUDE");
+        selectedPPM = getIntent().getExtras().getString("PPM");
+        selectedYear = Integer.parseInt(getIntent().getExtras().getString("YEAR"));
+
+        toolbar = (Toolbar) findViewById(R.id.graph_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HistoricalReportActivity.this, HomeActivity.class);
+                intent.putExtra("USER", user);
+                startActivity(intent);
+            }
+        });
+
+        graphInfo = (TextView) findViewById(R.id.textview_graph_info);
+        graphInfo.setText("\n\nLocation: " + latitude + "," + longitude
+                + "\n\nSelected PPM: " + selectedPPM
+                + "\n\nYear: " + selectedYear);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(1, 25),
+                new DataPoint(2, 10),
+                new DataPoint(3, 5),
+                new DataPoint(4, 13),
+                new DataPoint(5, 6),
+                new DataPoint(6, 6),
+                new DataPoint(7, 10),
+                new DataPoint(8, 3),
+                new DataPoint(9, 1),
+                new DataPoint(10, 8),
+                new DataPoint(11, 11),
+                new DataPoint(12, 20)
+        });
+        graph.addSeries(series);
     }
 
 }
--- WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/HistoricalReportParametersActivity.java
+++ WaterSourcingProject/app/src/main/java/edu/gatech/group16/watersourcingproject/controller/HistoricalReportParametersActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import edu.gatech.group16.watersourcingproject.model.User;
 public class HistoricalReportParametersActivity extends AppCompatActivity implements View.OnClickListener{
     private User user;
     private Spinner ppmSpinner, yearSpinner;
+    private EditText latitude, longitude;
     private List<String> ppmOptions = new ArrayList<>();
     private List<Integer> yearOptions = new ArrayList<>();
 
@@ -26,10 +28,13 @@ public class HistoricalReportParametersActivity extends AppCompatActivity implem
         setContentView(R.layout.activity_historical_report_parameters);
         user = (User) getIntent().getSerializableExtra("USER");
         findViewById(R.id.submit_button).setOnClickListener(this);
+        latitude = (EditText) findViewById(R.id.text_latitude);
+        longitude = (EditText) findViewById(R.id.text_longitude);
+        ppmSpinner = (Spinner) findViewById(R.id.spinner_ppm);
+        yearSpinner = (Spinner) findViewById(R.id.spinner_year);
 
         ppmOptions.add("Virus PPM");
         ppmOptions.add("Contaminant PPM");
-        ppmSpinner = (Spinner) findViewById(R.id.spinner_ppm);
         ArrayAdapter<String> adaptPPM = new ArrayAdapter(this, android.R.layout.simple_spinner_item, ppmOptions);
         ppmSpinner.setAdapter(adaptPPM);
 
@@ -37,7 +42,6 @@ public class HistoricalReportParametersActivity extends AppCompatActivity implem
             int currentYear = Calendar.getInstance().get(Calendar.YEAR);
             yearOptions.add(currentYear - i);
         }
-        yearSpinner = (Spinner) findViewById(R.id.spinner_year);
         ArrayAdapter<Integer> adaptYear = new ArrayAdapter(this, android.R.layout.simple_spinner_item, yearOptions);
         yearSpinner.setAdapter(adaptYear);
 
@@ -46,8 +50,16 @@ public class HistoricalReportParametersActivity extends AppCompatActivity implem
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.submit_button) {
+            String selectedLatitude = latitude.getText().toString();
+            String selectedLongitude = longitude.getText().toString();
+            String selectedPPM = ppmSpinner.getSelectedItem().toString();
+            String selectedYear = yearSpinner.getSelectedItem().toString();
             Intent intent = new Intent(this, HistoricalReportActivity.class);
             intent.putExtra("USER", user);
+            intent.putExtra("LATITUDE", selectedLatitude);
+            intent.putExtra("LONGITUDE", selectedLongitude);
+            intent.putExtra("PPM", selectedPPM);
+            intent.putExtra("YEAR", selectedYear);
             startActivity(intent);
         }
     }
